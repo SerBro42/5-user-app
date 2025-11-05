3952f8a331b2749500f08565a316700c81a0241a
Sergey Brovko <serbro7@gmail.com>
2025-11-05 18:27:15 +0100

    GET all users from back-end works. Pipe method. HttpClient

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,14 @@ export class UserService {
 
   private users: User[] = [];
 
+  constructor(private http: HttpClient) {}
+
   findAll(): Observable<User[]> {
-    return of(this.users);
+    //return of(this.users);
+    //We use the pipe method to cast our GET result into a different type of observable. The back-end emits a JSON, and as soon
+    //as we get it, we tranform it into an array of User-type objects.
+    return this.http.get('http://localhost:8080/api/users').pipe(
+      map((users:any) => users as User[]),
+    );
   }
 }
