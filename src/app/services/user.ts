181d1074c3ea5a6fca1f106e01988925268c0edf
Sergey Brovko <serbro7@gmail.com>
2025-11-05 18:31:04 +0100

    Alternative to pipe method for casting User[]

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -14,10 +14,7 @@ export class UserService {
 
   findAll(): Observable<User[]> {
     //return of(this.users);
-    //We use the pipe method to cast our GET result into a different type of observable. The back-end emits a JSON, and as soon
-    //as we get it, we tranform it into an array of User-type objects.
-    return this.http.get('http://localhost:8080/api/users').pipe(
-      map((users:any) => users as User[]),
-    );
+    //Method alternative to pipe. Using get<...> with User[] to achieve the same result.
+    return this.http.get<User[]>('http://localhost:8080/api/users');
   }
 }
