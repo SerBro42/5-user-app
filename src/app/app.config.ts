2e066ea351a70a6c1d82e04eb5154fabbcea969c
Sergey Brovko <serbro7@gmail.com>
2025-12-01 13:22:26 +0100

    Redux installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token-interceptor';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideStore()
+]
 };
