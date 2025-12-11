734d9787d6a629013a337aea750dc339f0242449
Sergey Brovko <serbro7@gmail.com>
2025-12-11 17:41:43 +0100

    Integrating Store into auth service

--- src/app/components/user-app.ts
+++ src/app/components/user-app.ts
@@ -37,22 +37,18 @@ export class UserAppComponent implements OnInit {
           //The original line, featuring atob and JSON.parse, was transferred to AuthService. This was done to make the code mor reusable and more modular.
           const payload = this.authService.getPayLoad(token);
 
-          //Having parsed the payload, we extract relevant data from it and save them in sessionStorage. You might want to declare isAuth
-          //as 'true' instead of payload.isAuth. Turns out, I had to do precisely that (28/11/2025)
-          const user = { username: payload.sub };
-          const login = {
-            user,
-            isAuth: true,
-            isAdmin: payload.isAdmin
-          };
           //sessionStorage only stores String-type data, and 'login' is an object. We must transform it into String first. We do it by means 
           //of JSON.stringify().
           //We decided to move the line that saves 'login' from here to AuthService
           this.authService.token = token;
-          this.authService.user = login;
-          this.router.navigate(['/users/page/0']);
-          //This line was added for debugging purposes. Remove when no longer needed.
-          console.log(payload);
+          //We decided to save some variables by declaring the value of 'user' here,
+          this.authService.user = {
+            user: { username: payload.sub },
+            isAuth: true,
+            isAdmin: payload.isAdmin
+          };
+          
+          this.router.navigate(['/users']);
         },
         error: error => {
           if (error.status == 401) {
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { login, logout } from '../store/auth/auth.actions';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +11,17 @@ export class AuthService {
 
   private url: string = 'http://localhost:8080/login';
 
-  //our 'token' can either exist (string) or not (undefined). This will come in handy later, to validate it as undefined.
-  private _token: string | undefined;
+  //This is the 'login' aka 'user' State. Here, in the Service, we give it the default values. From now on, the Auth will be managed by
+  //auth store.
+  private _user: any;
 
-  //This is the 'login' aka 'user' State. Here, in the Service, we give it the default values. In UserAppComponent we assign it values.
-  private _user: any = {
-    isAuth: false,
-    isAdmin: false,
-    user: undefined
-  }
-
-  constructor(private http: HttpClient) {}
+  constructor(
+    private store: Store<{auth: any}>,
+    private http: HttpClient) {
+      this.store.select('auth').subscribe(state => {
+        this._user = state;
+      })
+    }
 
   loginUser({ username, password }: any): Observable<any> {
     return this.http.post<any>(this.url, { username, password });
@@ -27,34 +29,23 @@ export class AuthService {
 
   //'login' comes from UserAppComponent
   set user(user: any) {
-    this._user = user;
+    this.store.dispatch(login({ login: user }));
     sessionStorage.setItem('login', JSON.stringify(user));
   }
 
   get user() {
-    if(this._user.isAuth) {
-      return this._user;
-    } else if (sessionStorage.getItem('login') != null) {
-      this._user = JSON.parse(sessionStorage.getItem('login') || '{}');
-      return this._user; 
-    }
+    //The user will be obtained from the Store
     return this._user;
   }
   
   //'token' comes from UserAppComponent
   set token(token: string) {
-    this._token = token;
     sessionStorage.setItem('token', token);
   }
 
+  //Now, the token comes from sessionStorage
   get token() {
-    if(this._token != undefined) {
-      return this._token;
-    } else if (sessionStorage.getItem('token') != null) {
-      this._token = sessionStorage.getItem('token') || '';
-      return this._token;
-    }
-    return this._token!;
+    return sessionStorage.getItem('token')!;
   }
 
   getPayLoad(token: string) {
@@ -75,12 +66,7 @@ export class AuthService {
   }
 
   logout() {
-    this._token = undefined;
-    this._user = {
-      isAuth: false,
-      isAdmin: false,
-      user: undefined
-    };
+    this.store.dispatch(logout());
     sessionStorage.removeItem('login');
     sessionStorage.removeItem('token');
   }
